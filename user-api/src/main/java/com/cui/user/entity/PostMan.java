53a94960681cc660091cb5c314f7edfa71582387
cuihongmin <1151684841@qq.com>
2022-08-02 08:57:51 +0800

    2222

--- user-api/src/main/java/com/cui/user/entity/PostMan.java
+++ user-api/src/main/java/com/cui/user/entity/PostMan.java
@@ -6,5 +6,22 @@ package com.cui.user.entity;
  * @Date 2022/8/1 17:17
  */
 public class PostMan {
+    private String username;
+    private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
